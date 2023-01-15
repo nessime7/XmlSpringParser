01fb33cfb1be05c806611e194a954c149dfb1801
nessime7 <sara.przebinda@gmail.com>
2023-01-15 01:30:37 +0100

    1

--- src/test/java/XmlSpringParser/XmlControllerTest.java
+++ src/test/java/XmlSpringParser/XmlControllerTest.java
@@ -1,4 +1,14 @@
+package XmlSpringParser;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class XmlControllerTest {
-  
+
+    @Test
+    void should_return_users(){
+
+
+    }
 }
