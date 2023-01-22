132b4047c42c012188c378545bcdc9dcc4e3a5c6
nessime7 <sara.przebinda@gmail.com>
2023-01-22 17:32:18 +0100

    showtime

--- src/main/java/XmlSpringParser/model/XmlUsers.java
+++ src/main/java/XmlSpringParser/model/XmlUsers.java
@@ -6,7 +6,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
-//
 @XmlRootElement(name = "users")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class XmlUsers {
