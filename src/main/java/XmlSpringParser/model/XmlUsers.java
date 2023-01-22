f71d18cf5faa7e40b7a929f0799dc94b16460967
nessime7 <sara.przebinda@gmail.com>
2023-01-22 15:53:45 +0100

    xml-parser-implementation

--- src/main/java/XmlSpringParser/model/XmlUsers.java
+++ src/main/java/XmlSpringParser/model/XmlUsers.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
-//
+
 @XmlRootElement(name = "users")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class XmlUsers {
