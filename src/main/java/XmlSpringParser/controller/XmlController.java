8645a4954e601561b95c4f0e1786417876d60666
nessime7 <sara.przebinda@gmail.com>
2023-01-22 16:47:57 +0100

    xml-parser-implementation

--- src/main/java/XmlSpringParser/controller/XmlController.java
+++ src/main/java/XmlSpringParser/controller/XmlController.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
-//
+
 @RestController
 public class XmlController {
 
@@ -27,45 +27,3 @@ public class XmlController {
         return ResponseEntity.ok(result);
     }
 }
-
-//    final var jaxbContext = JAXBContext.newInstance(XmlUsers.class);
-//    final var jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-//    final var users = (XmlUsers) jaxbUnmarshaller.unmarshal(file.getInputStream());
-//    final var result = users.getUsers();
-//    final var objectMapper = new ObjectMapper();
-//        return objectMapper.writeValueAsString(result);
-
-// My first converter and it returns text
-//        // convertFile pusty
-//        File convertFile = new File("C:/Users/sarap/data/" + file.getOriginalFilename());
-//        // stworzenie
-//        convertFile.createNewFile();
-//        // przetransferowanie file do convertFile
-//    //    file.transferTo(convertFile);
-//        // uruchomienie parsera na convertFile
-//        final var parsedMessage = new XmlParser().parser(convertFile);
-//        // wyświetlenie wiadomości
-//   //     new XmlPrinter().print(parsedMessage);
-//        // próba
-//        try (FileOutputStream fout = new FileOutputStream(parsedMessage)) {
-//            fout.write(file.getBytes());
-//        } catch (Exception exe) {
-//            exe.printStackTrace();
-//        }
-//        return parsedMessage;
-//    }
-//}
-
-// Converter to copy my file to different direction
-//    @PostMapping("/upload")
-//    public String handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
-//        File convertFile = new File("C:/Users/sarap/data/" + file.getOriginalFilename());
-//        convertFile.createNewFile();
-//        try (FileOutputStream fout = new FileOutputStream(convertFile)) {
-//            fout.write(file.getBytes());
-//
-//        }
-//        catch (Exception exe) {
-//            exe.printStackTrace();
-//        }
-//        return "File has uploaded successfully";
--- src/main/java/XmlSpringParser/model/XmlUser.java
+++ src/main/java/XmlSpringParser/model/XmlUser.java
@@ -1,7 +1,7 @@
 package XmlSpringParser.model;
 
 import javax.xml.bind.annotation.*;
-//
+
 @XmlRootElement(name = "user")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class XmlUser {
