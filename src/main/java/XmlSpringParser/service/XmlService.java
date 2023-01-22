9d18c3d5fe14d41e86129bad058df2a66eb2c12f
nessime7 <sara.przebinda@gmail.com>
2023-01-22 15:52:56 +0100

    xml-parser-implementation

--- src/main/java/XmlSpringParser/service/XmlService.java
+++ src/main/java/XmlSpringParser/service/XmlService.java
@@ -8,7 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
-
+//
 @Service
 public class XmlService {
     public String handleFileUpload(MultipartFile file) throws JAXBException, IOException {
--- src/test/java/com/XmlSpringParser/TestUtils.java
+++ src/test/java/com/XmlSpringParser/TestUtils.java
@@ -6,7 +6,7 @@ import org.springframework.util.ResourceUtils;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
-
+//
 public class TestUtils {
     public static String getRequestBodyFromFile(String filename, String context) throws IOException {
         final var file = ResourceUtils.getFile(String.format("classpath:%s/%s", context, filename));
