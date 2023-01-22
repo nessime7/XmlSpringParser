38ab71f790b1022c2cbb8962acf63cae70827a08
nessime7 <sara.przebinda@gmail.com>
2023-01-22 15:53:45 +0100

    PR preparation

---
+++ src/main/java/XmlSpringParser/model/XmlUser.java
@@ -0,0 +1,38 @@
+package XmlSpringParser.model;
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name = "user")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class XmlUser {
+    @XmlElement(name = "name")
+    private String name;
+    @XmlElement(name = "email")
+    private String email;
+    @XmlElement(name = "username")
+    private String username;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
