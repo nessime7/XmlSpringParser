cd80178c5030ebbacc4fcd267491e58da97c48f7
nessime7 <sara.przebinda@gmail.com>
2023-01-28 14:45:03 +0100

    showtime

--- src/main/java/XmlSpringParser/controller/XmlController.java
+++ src/main/java/XmlSpringParser/controller/XmlController.java
@@ -1,7 +1,9 @@
 package XmlSpringParser.controller;
 
+import XmlSpringParser.model.User;
 import XmlSpringParser.service.XmlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class XmlController {
@@ -22,9 +25,9 @@ public class XmlController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<String> processXmlFile(@RequestParam("file") MultipartFile file) throws IOException, JAXBException {
+    public ResponseEntity<List<User>> processXmlFile(@RequestParam("file") MultipartFile file) throws IOException, JAXBException {
         final var result = xmlService.processXmlFile(file);
-        return ResponseEntity.ok(result);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
 
--- src/main/java/XmlSpringParser/service/XmlService.java
+++ src/main/java/XmlSpringParser/service/XmlService.java
@@ -1,22 +1,22 @@
 package XmlSpringParser.service;
 
 import XmlSpringParser.model.Document;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import XmlSpringParser.model.User;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.util.List;
+
 
 @Service
 public class XmlService {
-    public String processXmlFile(MultipartFile file) throws JAXBException, IOException {
+    public List<User> processXmlFile(MultipartFile file) throws JAXBException, IOException {
         final var jaxbContext = JAXBContext.newInstance(Document.class);
         final var jaxbUnmarshaller = jaxbContext.createUnmarshaller();
         final var users = (Document) jaxbUnmarshaller.unmarshal(file.getInputStream());
-        final var result = users.getUsers();
-        final var objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(result);
+        return users.getUsers();
     }
 }
